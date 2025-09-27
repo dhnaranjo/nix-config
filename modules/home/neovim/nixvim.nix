db427ce29d8076714da960650e5f4ddc8f5b7d0a
Desmond Naranjo <itsdesmond@hey.com>
2025-09-27 11:10:48 -0500

    Remove Manix from Nixvim
    It don't damn work.

--- modules/home/neovim/nixvim.nix
+++ modules/home/neovim/nixvim.nix
@@ -4,7 +4,6 @@
   vimAlias = true;
   enableMan = true;
 
-
   # Theme
   colorschemes.tokyonight.enable = true;
 
@@ -58,7 +57,6 @@
       };
       extensions = {
         file-browser.enable = true;
-        manix.enable = true;
       };
     };
 
@@ -88,55 +86,4 @@
       key = "<leader>gg";
     }
   ];
-
-  dependencies = {
-    manix.enable = true;
-  };
-
-  extraConfigLua = ''
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        -- Wait for telescope to be available
-        vim.schedule(function()
-          local has_telescope, telescope = pcall(require, 'telescope')
-          if not has_telescope then return end
-          
-          -- Load the extension first
-          telescope.load_extension('manix')
-          
-          -- Now monkey-patch the search function
-          local actions = require("telescope.actions")
-          local actions_state = require("telescope.actions.state")
-          
-          -- Override the problematic attach_mappings
-          local manix_mod = require("telescope-manix")
-          local original_search = manix_mod.search
-          
-          manix_mod.search = function(opts)
-            opts = opts or {}
-            opts.attach_mappings = function(buf)
-              actions.select_default:replace(function()
-                local entry = actions_state.get_selected_entry()
-                if entry and entry.display then
-                  actions.close(buf)
-                  -- Fix: ensure it's a valid string
-                  local text = tostring(entry.display)
-                  if text and text ~= "" then
-                    vim.api.nvim_put({ text }, "", true, true)
-                  else
-                    vim.notify("Invalid manix entry", vim.log.levels.WARN)
-                  end
-                else
-                  vim.notify("No entry selected", vim.log.levels.WARN)
-                end
-              end)
-              return true
-            end
-            
-            return original_search(opts)
-          end
-        end)
-      end
-    })
-  '';
 }
