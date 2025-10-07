259a0c0f31e3f3b3c881c971cd07f5b759ef331d
Desmond Naranjo <itsdesmond@hey.com>
2025-10-07 00:20:47 -0500

    Trying to sort darwin-excluding apps reqn homebrew

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -3,48 +3,52 @@
   # Nix packages to install to $HOME
   #
   # Search for packages here: https://search.nixos.org/packages
-  home.packages = with pkgs; [
-    omnix
+  home.packages =
+    with pkgs;
+    [
+      omnix
 
-    # Updating outdated Mac builtins
-    coreutils
-    findutils
-    gnused
-    gawk
-    gnugrep
-    curl
-    wget
-    openssh
-    git
-    rsync
-    gnutar
-    gzip
-    bzip2
-    xz
-    bash
-    zsh
-    less
+      # Updating outdated Mac builtins
+      coreutils
+      findutils
+      gnused
+      gawk
+      gnugrep
+      curl
+      wget
+      openssh
+      git
+      rsync
+      gnutar
+      gzip
+      bzip2
+      xz
+      bash
+      zsh
+      less
 
-    # Unix tools
-    ripgrep # Better `grep`
-    fd
-    sd
-    tree
-    gnumake
-    moreutils # `vipe` and others https://joeyh.name/code/moreutils/
+      # Unix tools
+      ripgrep # Better `grep`
+      fd
+      sd
+      tree
+      gnumake
+      moreutils # `vipe` and others https://joeyh.name/code/moreutils/
 
-    # Nix dev
-    cachix
-    nil # Nix language server
-    nix-info
-    nixpkgs-fmt
+      # Nix dev
+      cachix
+      nil # Nix language server
+      nix-info
+      nixpkgs-fmt
 
-    yt-dlp
-    aria2
+      yt-dlp
+      aria2
 
-    openscad-unstable
-    # orca-slicer needs to be installed with Homebrew
-  ];
+      openscad-unstable
+    ]
+    ++ lib.optionals (!pkgs.stdenv.isDarwin) [
+      orca-slicer
+    ];
 
   # Programs natively supported by home-manager.
   # They can be configured in `programs.*` instead of using home.packages.
