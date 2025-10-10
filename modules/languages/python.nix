73de056bdace4c6cb017389673b36ab4cab97f40
Desmond Naranjo <itsdesmond@hey.com>
2025-10-09 19:10:27 -0500

    Temporarily switch Python LSP from basedpyright to pyright
    Work around nixpkgs issue with basedpyright until fix propagates to nixos-unstable.
    See: https://github.com/NixOS/nixpkgs/pull/450202
    
    TODO: Revert to basedpyright on 2025-10-16

--- modules/languages/python.nix
+++ modules/languages/python.nix
@@ -9,7 +9,12 @@
     vim.languages.python = {
       enable = true;
       treesitter.enable = true;
-      lsp.enable = true;
+      lsp = {
+        enable = true;
+        # TODO (2025-10-16): Revert to basedpyright once fix propagates to nixos-unstable
+        # https://github.com/NixOS/nixpkgs/pull/450202
+        server = "pyright";
+      };
     };
   };
 
